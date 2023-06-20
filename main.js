85c7319862c9d0736a32906d9f7257f1bb3d4482
fishyfishfishfish <114358433+fishyfishfishfish@users.noreply.github.com>
2023-06-19 20:40:23 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,20 +1,32 @@
+noseX=0;
+noseY=0;
+marioX=325;
+marioY=325;
+img="";
+
 function preload() {
 	world_start = loadSound("world_start.wav");
 	setSprites();
 	MarioAnimation();
+    img=loadImage("mario.png")
 }
 
 function setup() {
 	canvas = createCanvas(1240,336);
 	canvas.parent('canvas');
 	instializeInSetup(mario);
+	video=createCapture(VIDEO);
+	video.size(1200,300);
+	posenet=ml5.posenet(video,modelLoaded);
 }
 
 function draw() {
 	game()
 }
 
-
+function modelLoaded(){
+	console.log('model is loaded');
+}
 
 
 
